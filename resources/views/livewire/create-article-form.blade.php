9b077002161b2145e87b7bdbc63cda9e99abbc81
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-06-18 22:05:33 +0200

    VERIFICARE LA VALIDAZIONE DELLA CATEGORIA NEL FORM , manca collegamento col db, aggiungere middleware

--- resources/views/livewire/create-article-form.blade.php
+++ resources/views/livewire/create-article-form.blade.php
@@ -1,27 +1,48 @@
-<form class="bg-body-tertiary shadow rounded p-5 my-5">
+@if (session()->has('success'))
+    <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+    </div>
+@endif
 
-  <div class="mb-3">
-    <label for="title" class="form-label">Titolo</label>
-    <input type="text" class="form-control" id="title" wire:model="title">
-  </div>
-  <div class="mb-3">
-    <label for="description" class="form-label">Descrizione</label>
-    <textarea id="description" cols="30" rows="10" class="form-control" wire:model="description"></textarea>
-  </div>
-  <div class="mb-3">
-    <label class="form-label" for="price">Prezzo</label>  
-    <input type="text" class="form-control" id="price" wire:model="price">
-  </div>
-  <div class="mb-3">
-    <select class="form-control" id="category" wire:model="category">
-        <option label disabled>Seleziona categoria</option>
-        @foreach($categories as $category)
-            <option value="{{$category->id}}">{{$category->name}}</option>
-        @endforeach
-    </select>
-  </div>
-  <div class="d-flex justify-content-center">
-    <button type="submit" class="btn btn-primary">Crea</button>
-  </div>
-  
+<form class="bg-body-tertiary shadow rounded p-5 my-5" wire:submit="store">
+
+    <div class="mb-3">
+        <label for="title" class="form-label">Titolo</label>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+            wire:model.blur="title">
+        @error('title')
+            <p class="fst-italic text-danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="description" class="form-label">Descrizione</label>
+        <textarea id="description" cols="30" rows="10" class="form-control @error('description') is-invalid @enderror"
+            wire:model.blur="description"></textarea>
+        @error('description')
+            <p class="fst-italic text-danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="price">Prezzo</label>
+        <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
+            wire:model.blur="price">
+        @error('price')
+            <p class="fst-italic text-danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <select class="form-control @error('category') is-invalid @enderror" id="category" wire:model.blur="category">
+            <option label disabled>Seleziona categoria</option>
+            @foreach ($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+        </select>
+        @error('category')
+            <p class="fst-italic text-danger">{{ $message }}</p>
+        @enderror
+    </div>
+    <div class="d-flex justify-content-center">
+        <button type="submit" class="btn btn-primary">Crea</button>
+    </div>
+
 </form>
