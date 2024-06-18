bb39a572db3edb2a4a09caf41107d2abdc058ddb
Sist95 <emanuele.sisto@icloud.com>
2024-06-18 19:51:05 +0200

    Creato modello Categorie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,36 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    public $categories = [
+        'elettronica',
+        'abbigliamento',
+        'salute e bellezza',
+        'casa e giardino',
+        'giocattoli',
+        'sport',
+        'animali domestici',
+        'libri e riviste',
+        'accessori',
+        'motori'
+      ];
     /**
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+     {
+        foreach($this->categories as $category){
+            Category::create([
+            'name'=>$category
+            ]);
+        }
+    
     }
+    
 }
+   
+
