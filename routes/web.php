1176c6872ed81137565bb230d44c0a4cce0cf1c2
Sist95 <emanuele.sisto@icloud.com>
2024-06-19 18:26:52 +0200

    prova fasulla

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::get('/create/article', [ArticleController::class,'create'])->name('create
 Route::get('/article/index', [ArticleController::class,'index'])->name('article.index');
 
 
+bla bla 
+
+
