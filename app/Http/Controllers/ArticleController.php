50b172cc86fd714b6cf0c8e633437418854ae564
Rita Ciociola <rita.ciociola91@gmail.com>
2024-06-19 18:24:30 +0200

    eliminata riga

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -31,7 +31,5 @@ class ArticleController extends Controller implements HasMiddleware
     public function show(Article $article){
         return view('article.show',compact('article'));
     }
-
-    public function
 }   
 
