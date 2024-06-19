8ea361fd6d1bf7675cebf0244d48247639ef820a
Fiorenzo Napolitano <napfiore@libero.it>
2024-06-19 20:00:02 +0200

    creata la vista bycategory

--- resources/views/article/byCategory.blade.php
+++ resources/views/article/byCategory.blade.php
@@ -6,6 +6,20 @@
                 <h1 class="diplay-2">Annunci della categoria <span class="fst-italic fw-bold">{{$category->name}}</span></h1>
             </div>
         </div>
-    </div>
-
+        <div class="row justify-content-center align-items-center py-5">
+            @forelse
+            ($articles as $article) 
+             <div class="col-12 col-md-3">
+                <x-card :article="$article"/>
+             </div>
+            @empty
+            <div class="col-12 text-center">
+               <h3>Non sono ancora stati creati annunci per questa categoria</h3>
+               @auth 
+                <a class="btn btn-dark my-5" href="{{route('create.article')}}">Pubblica un annuncio</a>
+                @endauth
+            </div>
+             @endforelse
+        </div>
+     </div>
 </x-layout>
