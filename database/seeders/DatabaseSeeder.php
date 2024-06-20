b70f38e5aca200cc18f60989e326e5a38044b712
Giuseppe Esposito <giuseppeesposito1993@gmail.com>
2024-06-20 22:46:57 +0200

    risolto errore di sintassi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         'Animali domestici',
         'Libri e riviste',
         'Accessori',
-        'Motori'
+        'Motori',
       ];
     /**
      * Seed the application's database.
