5335ba0f3aae7500ceeec8ee3fb08bd14c86027e
Giuseppe Esposito <giuseppeesposito1993@gmail.com>
2024-06-21 01:04:33 +0200

    customizzati tutti i bottoni

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,7 +3,7 @@
     <div class="card-body">{{Str::limit($article->title, 30)}}</h4>
         <h6 class="card-subtitle text-body-secondary">{{$article->price}}</h6>
         <div class="d-flex flex-column justify-content-evenly align-items-center mt-5">
-            <a href="{{route('article.show',compact('article'))}}" class="buttonCustomPrimary text-white text-decoration-none">Mostra</a>
+            <a href="{{route('article.show',compact('article'))}}" class="buttonCustomPrimary text-decoration-none">Mostra</a>
             <a href="{{route('byCategory', ['category'=> $article->category->id])}}" class="buttonCustomInfo mt-2 text-decoration-none">{{$article->category->name}}</a>
         </div>
     </div>
