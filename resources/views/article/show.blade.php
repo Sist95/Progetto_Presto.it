a1ca53b3018e6010037fad20e34da69245b0474b
Sist95 <emanuele.sisto@icloud.com>
2024-06-20 19:59:02 +0200

    Prova di carousel nella card dell'articolo cliccando su mostra

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,38 +1,44 @@
 <x-layout>
-   <div class="row heigh-custom justify-content-center align-items-center text-center">
-    <div class="col-12">
-        <h1 class="display-4">Dettaglio dell'articolo: {{$article->title}}</h1>
-    </div>
-   </div>
-   <div class="row height-custom justify-content-cente py-5">
-    <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="https://picsum.photos/200" class="d-block w-100" alt="Immagine Casuale">
-          </div>
-          <div class="carousel-item">
-            <img src="https://picsum.photos/200" class="d-block w-100" alt="Immagine Casuale">
-          </div>
-          <div class="carousel-item">
-            <img src="https://picsum.photos/200" class="d-block w-100" alt="Immagine Casuale">
-          </div>
+    <div class="row heigh-custom justify-content-center align-items-center text-center">
+        <div class="col-12">
+            <h1 class="display-4">Dettaglio dell'articolo: {{ $article->title }}</h1>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
     </div>
-    <div class="col-12 col-md6 mb-3 height-custom text-center">
-        <h2 class="display-5"><span class="fw-bold">Titolo: </span>{{$article->title}}</h2>
+    <div class="row height-custom justify-content-center py-5">
+        <div class="col-12 col-md-6">
+
+          <div id="carouselExample" class="carousel slide carousel-custom">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://picsum.photos/500" class="d-block w-100 " alt="Immagine Casuale">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/500" class="d-block w-100" alt="Immagine Casuale">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/500" class="d-block w-100" alt="Immagine Casuale">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    
+    <div class="col-12 col-md-6 mb-3 height-custom text-center">
+        <h2 class="display-5"><span class="fw-bold">Titolo: </span>{{ $article->title }}</h2>
         <div class="d-flex flex-column justify-content-center h-75">
-            <h4 class="fw-bold">Prezzo: {{$article->price}}</h4>
+            <h4 class="fw-bold">Prezzo: {{ $article->price }}</h4>
             <h5>Descrizione:</h5>
-            <p>{{$article->description}}</p>
+            <p>{{ $article->description }}</p>
         </div>
     </div>
+  </div>
 </x-layout>
