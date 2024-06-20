3b9908f5c5cb4fd04e71bc04abb176009a704b7e
Fiorenzo Napolitano <napfiore@libero.it>
2024-06-20 20:25:32 +0200

    prova di visualizzazione

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class="container pt-5">
-        <div class="row justify-content-center">
+    <div class="container pt-5 ">
+        <div class="row justify-content-center ">
             <div class="col-12 text-center">
                 <h1 class="display-4 pt-5">Pubblica un articolo</h1>
             </div>
