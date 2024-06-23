2a6b92891474cffbc22ef758af4cf54131c6e9ab
Rita Ciociola <rita.ciociola91@gmail.com>
2024-06-23 18:35:13 +0200

    possibilità di accettare o rifiutare articolo inserito

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/show/article/{article}', [ArticleController::class, 'show'])->name(
 Route::get('/category/{category}', [ArticleController::class, 'byCategory'])->name('byCategory');
 
 Route::get('revisor/index', [RevisorController::class, 'index'])->name('revisor.index');
+
+Route::patch('/accept/{article}', [RevisorController::class, 'accept'])->name('accept');
+
+Route::patch('/reject/{article}', [RevisorController::class, 'reject'])->name('reject');
