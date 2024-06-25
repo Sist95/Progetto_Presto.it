513ad0a178b518f8ae5b333bbc8488ab0e2c1284
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-06-25 19:55:07 +0200

    creata rotta searcheable, aggiunto barra di ricerca nella navbar

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::patch('/reject/{article}', [RevisorController::class, 'reject'])->name('r
 Route::get('/revisor/request', [RevisorController::class, 'becomeRevisor'])->middleware('auth')->name('become.revisor');
 
 Route::get('/make/revisor/{user}', [RevisorController::class, 'makeRevisor'])->name('make.revisor');
+
+Route::get('/search/article', [PublicController::class, 'searchArticles'])->name('article.search');
+
