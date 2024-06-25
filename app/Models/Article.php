4a45bc7586e11af9fa6d99a4f76731c867a4bd2d
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-06-25 19:46:13 +0200

    creata funzione toSearcheableArray

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
+    use Searchable;
     use HasFactory;
 
     protected $fillable =[
@@ -38,5 +40,14 @@ class Article extends Model
 
     public static function toBeRevisedCount(){
         return Article::where('is_accepted', null)->count();
+    }
+
+    public function toSearchableArray(){
+        return[
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'description'=>$this->description,
+            'category'=>$this->category,
+        ];
     }
 }
