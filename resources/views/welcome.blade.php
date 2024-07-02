4aa88f0ae66ba9f73238d7e75e66c44e5e5d48f6
Giuseppe Esposito <giuseppeesposito1993@gmail.com>
2024-07-02 23:21:48 +0200

    modificata altezza del body portando la hero section più in basso e cambiato il bottone della hero section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <section class="hero rounded-5 rounded-top-0 py-5">
+    <section class="hero rounded-4 py-5">
         <div class="row">
         <div class="col-6 px-5">
             <p >{{__('ui.hello')}} </p>
@@ -9,11 +9,19 @@
             <h2>{{env('APP_NAME')}}</h2>
         </div>
           </div>
-          <a href="{{route('login')}}" class="buttonAccedi px-5 text-decoration-none" >{{__('ui.Accedi')}}
+          @auth
+            <a href="{{route('create.article')}}" class="buttonHero px-5 text-decoration-none" >{{__('ui.Inserisci annuncio')}}
+                <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+                <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+                </svg>
+            </button></a>
+          @else
+          <a href="{{route('register')}}" class="buttonAccedi px-5 text-decoration-none" >{{__('ui.Registrati')}}
             <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
-              <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+            <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
             </svg>
-          </button></a>
+        </button></a>
+          @endauth
 
         <div class="waves"></div>
       </section>
@@ -30,17 +38,17 @@
                 {{session('errorMessage')}}
               </div>
             @endif
-        <div class="col-10">
+        {{-- <div class="col-10">
 
-                {{-- <h1 class="display-1">{{env('APP_NAME')}}</h1> --}}
+                <h1 class="display-1">{{env('APP_NAME')}}</h1>
                 <div class="d-flex justify-content-center my-3 mb-5">
                     @auth
                         <a class="buttonCustomPrimary text-decoration-none" href="{{route('create.article')}}">{{__('ui.Inserisci annuncio')}}</a>
                     @endauth
                 </div>
-            </div>
+            </div> --}}
         </div>
-        <div class="row justify-content-center align-items-center">
+        <div class="row justify-content-center align-items-center pt-5">
         @forelse($articles as $article)
             <div class="col-12 col-md-6 col-xl-4">
                 <x-card :article="$article" />
