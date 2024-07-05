b180fe03742882cc5d6dfdc2c747df9a9c7af399
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-07-05 14:45:36 +0200

    sistemata hero section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,35 @@
 <x-layout>
         <section class="hero rounded-4 py-5">
             <div class="row">
-            <div class="col-6 px-5">
+            <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center text-center">
                 <p >{{__('ui.hello')}} </p>
 
             </div>
-            <div class="col-6">
+            <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center text-center">
                 <h2>{{env('APP_NAME')}}</h2>
             </div>
-            </div>
+
             @auth
+               <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center text-center">
                 <a href="{{route('create.article')}}" class="buttonHero px-5 text-decoration-none" >{{__('ui.Inserisci annuncio')}}
                     <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
                     <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
                     </svg>
                 </button></a>
+               </div>
             @else
-            <a href="{{route('register')}}" class="buttonHero buttonAccedi px-5 text-decoration-none" >{{__('ui.Registrati')}}
-                <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
-                <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
-                </svg>
-            </button></a>
+            <div class="col-12 col-lg-4 d-flex align-items-center justify-content-center text-center">
+                <a href="{{route('register')}}" class="buttonHero buttonAccedi px-5 text-decoration-none" >{{__('ui.Registrati')}}
+                    <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
+                    <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" fill-rule="evenodd"></path>
+                    </svg>
+                </button></a>
+            </div>
+        </div>
+
             @endauth
 
-            <div class="waves"></div>
+            {{-- <div class="waves"></div> --}}
         </section>
     <div class="container-fluid text-center">
         <div class="row justify-content-center align-items-center">
