e8dec6d679514ba792de838e74f52c54cc7d2abe
Sist95 <emanuele.sisto@icloud.com>
2024-07-06 17:01:42 +0200

    cambiati nomi degli articoli in annunci, provata favicion ma ci vuole unimmagine piccola, rimossa

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -30,13 +30,13 @@ class RevisorController extends Controller
     public function accept(Article $article)
     {
         $article->setAccepted(true);
-        return redirect()->back()->with('message_accepted',"Hai accettato l'articolo $article->title");
+        return redirect()->back()->with('message_accepted',"Hai accettato l'annuncio $article->title");
     }
 
     public function reject(Article $article)
     {
         $article->setAccepted(false);
-        return redirect()->back()->with('message_rejected',"Hai rifiutato l'articolo $article->title");
+        return redirect()->back()->with('message_rejected',"Hai rifiutato l'annuncio $article->title");
        
     }
     public function review(Article $article){
