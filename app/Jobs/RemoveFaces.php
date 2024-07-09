22d40e1da426b8798eede2f8a97d3a46355016d4
Sist95 <emanuele.sisto@icloud.com>
2024-07-09 20:14:13 +0200

    Risolto problema con immagini

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -57,7 +57,7 @@ class RemoveFaces implements ShouldQueue
         
         $image = SpatieImage::load($srcPath);
         $image->watermark(
-            base_path('resources/img/smile.png'),
+            base_path('resources/img/LogoPresto.png'),
             AlignPosition::TopLeft,
             paddingX: $bounds[0][0],
             paddingY: $bounds[0][1],
